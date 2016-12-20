414cfc2424674b91e7dc5bd733e43c4f94f377c4
Wei Yang Wang <wei.yang.wang@cern.ch>
2016-12-20 10:05:31 +0100

    new configs with ZeroBias and High Multiplicity datasets.

--- data_config.py
+++ data_config.py
@@ -3,10 +3,10 @@ process = cms.Process("anaTree")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
-#data set with HM triggers '/HighMultiplicity*/Run2015B-PromptReco-v1/RECO'
+#data set with HM triggers '/HighMultiplicity/Run2015B-PromptReco-v1/RECO'
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-#	'/store/data/Run2015B/ZeroBias8/RECO/PromptReco-v1/000/251/721/00000/02CB06F2-3A2C-E511-BF63-02163E01399F.root',
+	'/store/data/Run2015B/ZeroBias8/RECO/PromptReco-v1/000/251/721/00000/02CB06F2-3A2C-E511-BF63-02163E01399F.root',
 #	'/store/data/Run2015B/HighMultiplicity/RECO/PromptReco-v1/000/251/096/00000/1C89895E-5626-E511-A518-02163E0119E7.root'
 	'/store/data/Run2015B/HighMultiplicity/RECO/PromptReco-v1/000/251/721/00000/00BA646E-D82B-E511-A218-02163E01439E.root'
         )
